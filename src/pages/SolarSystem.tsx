7986cf3bdaab49194691e9af16aef1574e051fd0
benji77fr <b.girardpro@gmail.com>
2022-09-07 17:58:49 +0200

    remove black background

--- src/pages/SolarSystem.tsx
+++ src/pages/SolarSystem.tsx
@@ -1,12 +1,12 @@
 import { Suspense, lazy } from 'react';
 import { Canvas } from '@react-three/fiber';
-import { Loader } from '@react-three/drei';
+
 
 const SceneSolarSystem = lazy(() => import('../components/SceneSolarSystem/SceneSolarSystem'));
 
 function SolarSystem() {
   return (
-    <Suspense fallback={<Loader />}>
+    <Suspense fallback={null}>
       <Canvas camera={{ position: [0, 10, 25], fov: 80, near: 0.1, far: 10000}}>
         <ambientLight />
         <SceneSolarSystem />
